64f03e5039083278cf18ba02b896615f67c1da8f
Ivan <ravenivan1@gmail.com>
2025-01-19 01:12:53 -0500

    pagnition finished

--- src/firestoreServices.js
+++ src/firestoreServices.js
@@ -1,24 +1,29 @@
 import { collection, getDocs, limit, orderBy, query, startAfter } from "firebase/firestore"
 import { db } from "./firebase"
 
-let lastVisible = null
 
-
-export const fetchTickets = async (pageSize) => {
+export const fetchTickets = async () => {
   try {
-    let q = query(collection(db, "tickets"), orderBy('createdAt', 'desc'), limit(pageSize));
-
-    if (lastVisible) {
-      q = query(q, startAfter(lastVisible))
-    }
+    let q = query(collection(db, "tickets"), orderBy('createdAt', 'desc'));
 
     const querySnapshot = await getDocs(q)
-    lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1]
-    
+
+    let ticketsGroup = []    
     const tickets = []
+
     querySnapshot.forEach((doc) => {
-      tickets.push({ id: doc.id, ...doc.data() })
+      ticketsGroup.push({ id: doc.id, ...doc.data() })
+
+      if (ticketsGroup.length === 3) {
+        tickets.push(ticketsGroup)
+        ticketsGroup = []
+      }
     })
+
+    if (ticketsGroup.length > 0) {
+      console.log(ticketsGroup)
+      tickets.push(ticketsGroup)
+    }
 
     console.log(tickets)
 
@@ -30,6 +35,32 @@ export const fetchTickets = async (pageSize) => {
   }
 }
 
-export const resetPagnitionState = () => {
-  lastVisible = null
-}
+// export const fetchTickets = async (pageSize) => {
+//   try {
+//     let q = query(collection(db, "tickets"), orderBy('createdAt', 'desc'), limit(pageSize));
+
+//     if (lastVisible) {
+//       q = query(q, startAfter(lastVisible))
+//     }
+
+//     const querySnapshot = await getDocs(q)
+//     lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1]
+    
+//     const tickets = []
+//     querySnapshot.forEach((doc) => {
+//       tickets.push({ id: doc.id, ...doc.data() })
+//     })
+
+//     console.log(tickets)
+
+//     return tickets
+
+//   } catch(error) {
+//     console.error("Error fetching tickets", error)
+//     throw error;
+//   }
+// }
+
+// export const resetPagnitionState = () => {
+//   lastVisible = null
+// }
